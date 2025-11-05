c0cf23ab652c35ca749a2ef4a8ffbb1fd3be3cd2
E <79379754+oech3@users.noreply.github.com>
2025-11-05 22:58:09 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -87,15 +87,7 @@ pub fn main() {
                 phf_map.entry("sha256sum", map_value.clone());
                 phf_map.entry("sha384sum", map_value.clone());
                 phf_map.entry("sha512sum", map_value.clone());
-                phf_map.entry("sha3sum", map_value_bits.clone());
-                phf_map.entry("sha3-224sum", map_value.clone());
-                phf_map.entry("sha3-256sum", map_value.clone());
-                phf_map.entry("sha3-384sum", map_value.clone());
-                phf_map.entry("sha3-512sum", map_value.clone());
-                phf_map.entry("shake128sum", map_value_bits.clone());
-                phf_map.entry("shake256sum", map_value_bits.clone());
                 phf_map.entry("b2sum", map_value.clone());
-                phf_map.entry("b3sum", map_value_b3sum);
             }
             _ => {
                 phf_map.entry(krate, map_value.clone());
