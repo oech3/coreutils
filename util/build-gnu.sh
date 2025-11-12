cafd81bc1be2979fbbbb588399513871e2cee263
E <79379754+oech3@users.noreply.github.com>
2025-11-12 14:34:03 +0900

    Update build-gnu.sh

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -132,17 +132,18 @@ cd -
 
 # Pass the feature flags to make, which will pass them to cargo
 "${MAKE}" MULTICALL=y PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}"
+# Hardlink binaries
+for binary in $(${UU_BUILD_DIR}/coreutils --list); do
+    (cd ${UU_BUILD_DIR} && ln -v coreutils "$binary")
+done
+# The GNU tests rename this script before running, to avoid confusion with the make target
+ln -v "${UU_BUILD_DIR}/install" "${UU_BUILD_DIR}/ginstall"
 touch g
 echo "stat with selinux support"
 ./target/debug/stat -c%C g || true
 rm g
 
-ln -v "${UU_BUILD_DIR}/install" "${UU_BUILD_DIR}/ginstall" # The GNU tests rename this script before running, to avoid confusion with the make target
-# Hardlink binaries
-for binary in $(${UU_BUILD_DIR}/coreutils --list); do
-    (cd ${UU_BUILD_DIR} && ln -v coreutils "$binary")
-done
-test -f "${UU_BUILD_DIR}/[" || (cd ${UU_BUILD_DIR} && ln -s "test" "[")
+
 
 ##
 
